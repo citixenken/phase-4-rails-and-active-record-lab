f1f3f27ffd645f1e8c5db429f0ba5d8816cf78a5
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-09-26 15:39:39 +0300

    commit tree 131e4e5f5113d3e4e7c4d4c07c377bbc7dc33ece

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Student.create([{first_name: "Eliud", last_name: "Kipchoge", grade: 99 }, {first_name: "Jim", last_name: "Butler", grade: 68}])
